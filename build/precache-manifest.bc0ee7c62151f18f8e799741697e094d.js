6c1ac96fa661c7280ca99048129afa9eaf9ad132
erickkipkoech <you@example.com>
2023-01-18 09:36:35 +0300

    reverting to previous

---
+++ build/precache-manifest.bc0ee7c62151f18f8e799741697e094d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0b0759f123bb5d389cd",
+    "url": "/portfolio/static/css/main.c9a59ebc.chunk.css"
+  },
+  {
+    "revision": "f0b0759f123bb5d389cd",
+    "url": "/portfolio/static/js/main.00935068.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "f91dd5dead4290f46559",
+    "url": "/portfolio/static/js/2.3aba89ac.chunk.js"
+  },
+  {
+    "revision": "a7204da3d550e57bdd2e5b57f9fa66f1",
+    "url": "/portfolio/static/media/Image.a7204da3.png"
+  },
+  {
+    "revision": "b98be5e3641a1f50b359bbf3af45f3a6",
+    "url": "/portfolio/static/media/Image-1.b98be5e3.png"
+  },
+  {
+    "revision": "8aadc4009f86187da3b247c066264cdb",
+    "url": "/portfolio/static/media/p5.8aadc400.jpg"
+  },
+  {
+    "revision": "463d0d3aa818c4254779bae1108375ab",
+    "url": "/portfolio/static/media/p4.463d0d3a.jpg"
+  },
+  {
+    "revision": "b45e860c8610421a5621ee3ca36a097b",
+    "url": "/portfolio/static/media/portfolio.b45e860c.jpg"
+  },
+  {
+    "revision": "2e3c32e9bf3e8397b85aa06ad8146853",
+    "url": "/portfolio/static/media/p3.2e3c32e9.jpg"
+  },
+  {
+    "revision": "0809e3e0ef561fdaba0115450733b55e",
+    "url": "/portfolio/index.html"
+  }
+];
